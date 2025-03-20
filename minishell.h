9a922e32d555bb42f62c596c5a5d2ede781f5dfe
Douglas Alves <rachebartm00s@proton.me>
2025-03-20 01:16:13 +0000

    incomplete header file

---
+++ minishell.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dopereir <dopereir@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/20 01:10:10 by dopereir          #+#    #+#             */
+/*   Updated: 2025/03/20 01:14:12 by dopereir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <signal.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <string.h>
+
+typedef struct s_meta_data
+{
+	char	*input;
+	char	*tokens[];
+}
+
+#endif
