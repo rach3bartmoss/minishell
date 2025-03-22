7847442c05f451e3a1f050baebeeaeb8a54c5ae8
Douglas Alves <rachebartm00s@proton.me>
2025-03-22 21:46:55 +0000

    changes

--- init_data.c
+++ init_data.c
@@ -6,7 +6,7 @@
 /*   By: dopereir <dopereir@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 09:52:03 by dopereir          #+#    #+#             */
-/*   Updated: 2025/03/20 09:54:34 by dopereir         ###   ########.fr       */
+/*   Updated: 2025/03/22 00:25:16 by dopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,21 @@ void	init_data(t_data *shell)
 	shell->fd_in = STDIN_FILENO;
 	shell->fd_out = STDOUT_FILENO;
 	shell->pid = 0;
+}
+
+t_command	**parse_input(t_data *shell)
+{
+	int	i;
+	t_command	**commands;
+
+	commands = malloc((shell->token_count + 1) * sizeof(t_command *));
+	if (!commands)
+		return (NULL);
+	i = 0;
+	while (i < shell->token_count)
+	{
+		strcpy(shell->tokens[i], commands[i]);
+		commands[i][strlen(shell->tokens[i] + 1)] = '\0';
+		i++;
+	}
 }
